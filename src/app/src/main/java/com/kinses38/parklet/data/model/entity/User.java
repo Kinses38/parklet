6fd14e76eff50937a342049a0b7498a4297f9b53
kinses38 <shaun.kinsella38@mail.dcu.ie>
2020-01-31 14:46:10 +0000

    Create basic user java object to allow signin and authentication

---
+++ src/app/src/main/java/com/kinses38/parklet/data/model/entity/User.java
@@ -0,0 +1,71 @@
+package com.kinses38.parklet.data.model.entity;
+
+import com.google.firebase.database.Exclude;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private String uuid, name, email;
+
+    @Exclude
+    private boolean isNew, isCreated, isAuthenticated;
+
+    public User(){
+        //empty constructor required for firebase
+    }
+
+    User(String uuid, String name, String email){
+        this.uuid = uuid;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public void setNew(boolean aNew) {
+        isNew = aNew;
+    }
+
+    public boolean isCreated() {
+        return isCreated;
+    }
+
+    public void setCreated(boolean created) {
+        isCreated = created;
+    }
+
+    public boolean isAuthenticated() {
+        return isAuthenticated;
+    }
+
+    public void setAuthenticated(boolean authenticated) {
+        isAuthenticated = authenticated;
+    }
+}
