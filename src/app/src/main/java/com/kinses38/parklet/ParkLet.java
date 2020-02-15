c92f6634e26fef5da6c67b1ae9a07b18cd605e2a
kinses38 <shaun.kinsella38@mail.dcu.ie>
2020-02-15 15:36:18 +0000

    Enable firebase persistence at application level for better bandwith performance

--- src/app/src/main/java/com/kinses38/parklet/ParkLet.java
+++ src/app/src/main/java/com/kinses38/parklet/ParkLet.java
@@ -3,11 +3,13 @@ package com.kinses38.parklet;
 import android.app.Application;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ParkLet extends Application {
 
     public void onCreate(){
         super.onCreate();
         FirebaseApp.initializeApp(this);
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }
 }
