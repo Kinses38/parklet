d8eba3fbd45613d1b3f83f9cb714beb5ce1ded58
Shaun Kinsella <shaun.kinsella38@mail.dcu.ie>
2020-02-17 15:21:45 +0000

    Create recyclerview and adapter implementation to display vehicles
    Add recyclerview to display vehicles and placeholder text for no vehicles
    
    Fix refactor renaming error

--- src/app/src/main/java/com/kinses38/parklet/view/ui/fragments/VehiclesFragment.java
+++ src/app/src/main/java/com/kinses38/parklet/view/ui/fragments/VehiclesFragment.java
@@ -11,50 +11,71 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.annotations.Nullable;
 import com.kinses38.parklet.R;
-import com.kinses38.parklet.utilities.InputHandler;
-import com.kinses38.parklet.viewmodels.VehiclesViewModel;
 import com.kinses38.parklet.data.model.entity.Vehicle;
 import com.kinses38.parklet.databinding.FragmentVehiclesBinding;
+import com.kinses38.parklet.utilities.InputHandler;
+import com.kinses38.parklet.utilities.VehicleAdapter;
+import com.kinses38.parklet.viewmodels.VehiclesViewModel;
 
 import java.util.List;
 
-public class VehiclesFragment extends Fragment  implements View.OnClickListener{
+public class VehiclesFragment extends Fragment implements View.OnClickListener {
 
     private VehiclesViewModel vehiclesViewModel;
     private FragmentVehiclesBinding vehiclesLandingBinding;
 
+    private RecyclerView recyclerView;
+    private VehicleAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         vehiclesLandingBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_vehicles, container, false);
-        vehiclesViewModel = new ViewModelProvider(this).get(VehiclesViewModel.class);
+        vehiclesViewModel = new ViewModelProvider(getActivity()).get(VehiclesViewModel.class);
 
+        initRecyclerView();
+        initVehicleObserver();
         //Bind this fragment to allow onClick binding
         vehiclesLandingBinding.setVehicleFrag(this);
         //Binding boolean value to hide form
         vehiclesLandingBinding.setFormClicked(false);
         vehiclesLandingBinding.setVehicleViewModel(vehiclesViewModel);
 
-        initVehicleObserver();
-
-
+        //TODO duplicate bug is caused by loop in vehicle repo. Redo DB schema
         return vehiclesLandingBinding.getRoot();
     }
 
     private void initVehicleObserver() {
         vehiclesViewModel.getVehicles().observe(getViewLifecycleOwner(), new Observer<List<Vehicle>>() {
             @Override
-            public void onChanged(List<Vehicle> vehicles) {
-                for(Vehicle v: vehicles){
-                    vehiclesLandingBinding.setVehicle(v);
-                    Log.i("FragVeh", v.getModel());
+            public void onChanged(@Nullable List<Vehicle> vehicles) {
+                if (!vehicles.isEmpty()) {
+                    vehiclesLandingBinding.setHasVehicle(true);
+                    adapter.refreshList(vehicles);
+                    recyclerView.setAdapter(adapter);
+                    Log.i("Livedata: ", String.valueOf(this.hashCode()));
+                }
+                else{
+                    vehiclesLandingBinding.setHasVehicle(false);
                 }
             }
         });
     }
 
-    public void onClick(View v){
+    private void initRecyclerView(){
+        adapter = new VehicleAdapter(getActivity());
+        recyclerView = vehiclesLandingBinding.getRoot().findViewById(R.id.vehicle_recycler);
+        layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+    }
+
+    public void onClick(View v) {
         switch (v.getId()) {
             case R.id.vehicle_form_toggle_button:
                 vehiclesLandingBinding.setFormClicked(!vehiclesLandingBinding.getFormClicked());
@@ -64,7 +85,8 @@ public class VehiclesFragment extends Fragment  implements View.OnClickListener{
                 Log.i("3rd button", "cancel");
                 InputHandler.hideKeyboard(getActivity());
                 vehiclesLandingBinding.setFormClicked(!vehiclesLandingBinding.getFormClicked());
-            default: break;
+            default:
+                break;
         }
     }
 }
